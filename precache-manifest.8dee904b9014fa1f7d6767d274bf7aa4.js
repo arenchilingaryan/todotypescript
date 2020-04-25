314a705494f612c52b0f9640ae9f6df73e179b1a
Aren <reactDOM.render@yandex.ru>
2020-04-25 23:38:52 +0300

    Updates

---
+++ precache-manifest.8dee904b9014fa1f7d6767d274bf7aa4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c4bd7a93ddf665165e7ce27b8805f11",
+    "url": "/todotypescript/index.html"
+  },
+  {
+    "revision": "13d676800577eb04eb11",
+    "url": "/todotypescript/static/css/main.5d81d118.chunk.css"
+  },
+  {
+    "revision": "9ffc873f8008862794d2",
+    "url": "/todotypescript/static/js/2.123754f4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todotypescript/static/js/2.123754f4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13d676800577eb04eb11",
+    "url": "/todotypescript/static/js/main.2e24712e.chunk.js"
+  },
+  {
+    "revision": "ad673e1bb9d1155ee965",
+    "url": "/todotypescript/static/js/runtime-main.2d320dd4.js"
+  }
+]);
